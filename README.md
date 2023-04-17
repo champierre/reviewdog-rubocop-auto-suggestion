1dbe01f76485146f426ac01cefef5b5af55a5a83
Junya Ishihara <junya@champierre.com>
2023-04-17 16:43:17 +0900

    Add scripts/content.js, manifest.json and README.md.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reviewdog Rubocop Auto Suggestion
+
+This Chrome Extension automatically fill the title and body of suggestions provided by [reviewdog/action-rubocop](https://github.com/reviewdog/action-rubocop).
---
+++ scripts/content.js
@@ -0,0 +1,14 @@
+const commentBodies = document.querySelectorAll('.comment-body');
+for (let i = 0; i < commentBodies.length; i++) {
+  const suggestionButton = commentBodies[i].querySelectorAll('.js-apply-suggestion-button')[0];
+  const p = commentBodies[i].querySelectorAll('p[dir="auto"]')[0];
+  if (suggestionButton && p) {
+    const title = `Fix rubocop error: ${p.childNodes[1].textContent}`;
+    const body = `${title}\n${p.childNodes[4].textContent}`;
+    suggestionButton.addEventListener('click', (el) => {
+      const selectMenu = el.target.nextElementSibling;
+      selectMenu.querySelectorAll('.js-suggestion-commit-title')[0].value = title;
+      selectMenu.querySelectorAll('.js-quick-submit')[0].value = body;
+    });
+  }
+}
